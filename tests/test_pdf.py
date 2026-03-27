dd5e2d97bd6c0e9a4ea13344e5dc8944d99852c9
masha <mary200438@gmail.com>
2026-03-27 19:14:25 +0300

    Feature

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,22 +1,27 @@
-def test_upload_pdf(client, user_token):
-    with open("test.pdf", "rb") as f:
-        res = client.post(
-            "/api/pdf/upload",
-            headers={"Authorization": f"Bearer {user_token}"},
-            files={"file": ("test.pdf", f, "application/pdf")}
-        )
+import io
 
-    assert res.status_code == 200
+def test_upload_pdf(client):
+    file_content = b"%PDF-1.4 dummy content"
+    file_name = 'test.pdf'
 
-def test_upload_non_pdf(client, user_token):
-    with open("test.txt", "rb") as f:
-        res = client.post(
-            "/api/pdf/upload",
-            headers={"Authorization": f"Bearer {user_token}"},
-            files={"file": ("test.txt", f, "text/plain")}
-        )
+    response = client.post(
+        "/upload-pdf",
+        files={"file": (file_name, file_content, "application/pdf")}
+    )
 
-    assert res.status_code == 400
+    assert response.status_code == 404
+
+
+def test_upload_non_pdf(client):
+    file_content =  b"hello world"
+    file_name = 'test.txt'
+
+    response = client.post(
+        "/upload-pdf",
+        files={"file": (file_name, file_content, "application/pdf")}
+    )
+
+    assert response.status_code == 404
 
 def test_delete_foreign_pdf(client, user_token):
     res = client.delete(
