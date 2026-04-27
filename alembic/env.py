5e7f797b3c57f407166a9474753f85f2880c2f3f
masha <mary200438@gmail.com>
2026-04-27 12:25:37 +0300

    Написан CI/CD

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 from app.database import Base
-import app.models
+import app.models  # noqa: F401
 
 config = context.config
 
