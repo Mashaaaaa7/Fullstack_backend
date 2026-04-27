917c564eeea4536093fa56421bd2d3f9900b9d76
masha <mary200438@gmail.com>
2026-04-27 12:21:20 +0300

    Написан CI/CD

--- app/main.py
+++ app/main.py
@@ -1,21 +1,17 @@
 from contextlib import asynccontextmanager
-from app.minio_client import ensure_bucket, MINIO_BUCKET_PDF
 import asyncio
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import engine
-from app.models import Base
+from app.minio_client import ensure_bucket, MINIO_BUCKET_PDF
 from app.services.qa_generator_service import QAGeneratorService
 from app.endpoints import auth, profile, pdf, admin
 from app.routers import dictionary, seo, landing
 
 
-Base.metadata.create_all(bind=engine)
-
 @asynccontextmanager
-async def lifespan(app):
+async def lifespan(app: FastAPI):
     try:
         ensure_bucket(MINIO_BUCKET_PDF)
     except Exception as e:
@@ -27,9 +23,10 @@ async def lifespan(app):
     except Exception as e:
         print(f"⚠️ QAGenerator не инициализирован: {e}")
 
-    app.state.qa_service = qa  # ← ЭТА строка обязательна
-
+    app.state.qa_service = qa
     yield
+
+
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
@@ -41,17 +38,18 @@ app.add_middleware(
 )
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
-app.include_router(profile.router,prefix="/api/profile", tags=["profile"])
-app.include_router(pdf.router, prefix="/api/pdf",  tags=["pdf"])
+app.include_router(profile.router, prefix="/api/profile", tags=["profile"])
+app.include_router(pdf.router, prefix="/api/pdf", tags=["pdf"])
 app.include_router(dictionary.router, prefix="/api/dictionary", tags=["dictionary"])
 app.include_router(seo.router, tags=["seo"])
 app.include_router(landing.router, tags=["landing"])
-
 app.include_router(admin.router, prefix="/api/admin", tags=["admin"])
+
 
 @app.get("/")
 def root():
     return {"message": "API работает"}
+
 
 @app.get("/health")
 async def health_check():
